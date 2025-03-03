ef0bc27e5eeed4eb6738c5b1f751cb24385d150b
michaelslice <mjsens523@gmail.com>
2025-03-02 18:51:22 -0600

    feat: Fix

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
 import * as THREE from 'three';
 
+// Scene
 const scene = new THREE.Scene();
+
+// Camera (FOV, Aspect Ratio, Near, Far)
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// WebGL renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
+// Game code
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x112233  } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 
+// Animation loop
 function animate() {
 
 	cube.rotation.x += 0.01;
